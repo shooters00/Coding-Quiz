645a278027466a85dc1339ad23d16eed87d9e8e4
shooters00 <shooters0000@gmail.com>
2021-06-13 19:44:22 -0400

    High scores saved locally and pulled back.  Still working on display issues.

--- assets/js/highscript.js
+++ assets/js/highscript.js
@@ -1,14 +1,40 @@
-var startButtonEl = document.getElementById("goBack");
+var backButtonEl = document.getElementById("goBack");
 var clearButtonEl = document.getElementById("clearHigh");
-var highScoreEl = document.querySelector(".highScoreList")
+var highScoreEl = document.querySelector(".highscoreList")
 
-startButtonEl.addEventListener("click", function() {
-    console.log("Back Button Clicked!")
+function Init() {
+    highScoreEl.innerhtml = "";
+    if (localStorage.getItem("highscores") === null) {
+        var noScoresEl = document.createElement("h1");
+        noScoresEl.setAttribute("class", "main");
+        highScoreEl.appendChild(noScoresEl);
+    } else {
+        console.log("In the else section");
+        highScores = JSON.parse(localStorage.getItem("highscores"));
+        console.log(highScores);
+        var scoreEl;
+        var first;
+        var number;
+        for (i=0; i < highScores.length; i++) {
+            scoreEl = document.createElement("li");
+            highScore = highScores[i].values;
+            //number = highScores.highScore;
+            console.log(highScore);
+            //scoreEl.textContent = first + " " + number;
+            //highScoreEl.appendChild(scoreEl);
+        }
+    }
+}
+
+Init();
+
+backButtonEl.addEventListener("click", function() {
+    console.log("Back Button Clicked!");
     window.open('index.html');
 });
 
 clearButtonEl.addEventListener("click", function() {
-    console.log("Clear Highscores Button Clicked!")
+    console.log("Clear Highscores Button Clicked!");
 
     //Remove highScores from local storage
     localStorage.removeItem("highScores");
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,17 @@
 var buttonEl = document.getElementById("start");
 var countdownEl = document.getElementById("countdown");
-var mainEl = document.querySelector(".main")
-var quizEl = document.querySelector(".quiz")
+var mainEl = document.querySelector(".main");
+var quizEl = document.querySelector(".quiz");
+var resultsEl = document.querySelector("#results");
+var formEl = document.querySelector("#myForm");
+var scoreEl = document.querySelector("#myScore");
+var initialEl;
 var quizQuestions = [];
 var thisQuestion = [];
-var myAnswer="";
-
+var myAnswer=[];
+var countdown = 5;
+var score=0;
+var highscore;
 
 //Things to load on page open
 function Init() {
@@ -14,24 +20,30 @@ function Init() {
 }
 
 //Function to fun when the start button is pushed to commence the game
-function startGame() {
+function startGame(i) {
+    //isWin = false;
     var count = 0;
+    
     //Need to hide the main element & show the quiz element
     mainEl.setAttribute("style", "display: none");
     quizEl.setAttribute("style", "display: block");
+    startCountdown();
 
-    //Need to request first question/answer from quiz objects function & increment each time 
     pullQuestion(count);
     displayQuestion(count);
     answerQuestion();
     checkAnswer();
+
+    //Need to request first question/answer from quiz objects function & increment each time 
+    //for (i=0; i < 10; i++) {
+
+    //}
+    //gameOver();
 }
 
 function pullQuestion(num) {
     //Put this in another function
     thisQuestion = quizQuestionsObject[num];
-    console.log("This Question: ");
-    console.log(thisQuestion);
     return thisQuestion;
 }
 
@@ -60,40 +72,111 @@ function answerQuestion() {
     return myAnswer;
 }
 
-function checkAnswer () {
-    console.log("Output thisQuestion");
+function checkAnswer() {
+    console.log("Output this Question");
     console.log(thisQuestion);
     console.log("Output myAnswer");
     console.log(myAnswer);
-
-    if (thisQuestion === myAnswer) {
+    /*
+    if (thisQuestion === myAnswer[num]) {
         alert("You got it right");
-    } else if (thisQuestion !== myAnswer) {
+        score = score + 10;
+    } else if (thisQuestion !== myAnswer[num]) {
         alert("You got it wrong");
-        //reduce timer
+        countdown = countdown - 10;
     } else {
-        return
     }
+    */
 }
 
-    //function setOpacity(event) {
-//
-  //  }
+function startCountdown() {
+    // Sets Countdown
+    var countdownInterval = setInterval(function() {
+        if (countdown >= 0) {
+            countdownEl.textContent = countdown;
+            countdown--;
+        } else {
+            console.log("countdown < 0");
+            clearInterval(countdownInterval);
+            gameOver();
+        }
+    }, 1000);
+}
+
+function gameOver() {
+    console.log("In Game Over function");
+    resultsEl.setAttribute("style", "display: block");
+    quizEl.setAttribute("style", "display: none");
+
+    scoreEl.textContent = score;
+
+    //var scoreEl = document.createElement("h1")
+    //var formEl = document.createElement("form");
+    initialsEl = document.createElement("input");
+    var submitBtnEl = document.createElement("input");
+
+    initialsEl.setAttribute("type", "text");
+    initialsEl.setAttribute("Initials", "text");
+    initialsEl.setAttribute("placeholder", "Your Initials Here");
+
+    submitBtnEl.setAttribute("type", "submit");
+    submitBtnEl.setAttribute("value", "Submit");
+
+    myForm.appendChild(initialsEl);
+    myForm.appendChild(submitBtnEl);
+    //resultsEl.appendChild(scoreEl);
+
+    submitBtnEl.addEventListener("click", submitScore);
+}
+
+function submitScore(event) {
+    console.log("I'm submitting scores");
+    event.preventDefault();
+    var thisHighScore = [
+        {
+            highName: initialsEl.value,
+            highScore: score
+        }
+        ];
+    var highScores = [];
+    console.log("This high score" + thisHighScore);
+
+    //localStorage.setItem("quiz", JSON.stringify(quiz));
+
     
-    //Don't need anymore.  Added 'for' loop above
-    //var li2 = document.createElement("li");
-    //var li3 = document.createElement("li");
-    //var li4 = document.createElement("li");
-    //console.log(thisQuestion.options[0]);
-    //li2.textContent = thisQuestion.options[1];
-    //li3.textContent = thisQuestion.options[2];
-    //li4.textContent = thisQuestion.options[3];
-    //quizEl.appendChild(li2);
-    //quizEl.appendChild(li3);
-    //quizEl.appendChild(li4);
+    if (localStorage.getItem("highscores") === null) {
+        highScores.unshift(thisHighScore);
+      } else {
+        highScores = JSON.parse(localStorage.getItem("highscores"));
+            if (highScores.length > 5) {
+                highScores.shift();
+                highScores.unshift(thisHighScore);
+                //var numbers = [];
+                //for (i=0: i < 5; i++) {
+                    //numbers.push(highScores[i].highScore);
+
+                    //if (thisHighScore.highScore >= highScores[i].highScore) {
+                        //highScores.splice(i, 1);
+                        
+                    //} else {
+                    //}
+            } else {
+                    highScores.unshift(thisHighScore);
+            }
+        }
+    
+      console.log("High scores" + highScores);
+      localStorage.setItem("highscores", JSON.stringify(highScores));
+      window.open("./high_scores.html");
+}
+
+Init();
+buttonEl.addEventListener("click", startGame);
 
 
-    //start time
+
+
+/*
 
     //If answer if correct (compare choice against quiQuestionsObject.answer), incrment i & continue
     
@@ -103,33 +186,8 @@ function checkAnswer () {
 
     //If time is up, escape game
 
-
-
-/*
-function startTimer() {
-    // Sets timer
-    timer = setInterval(function() {
-      timerCount--;
-      timerElement.textContent = timerCount;
-      if (timerCount >= 0) {
-        // Tests if win condition is met
-        if (isWin && timerCount > 0) {
-          // Clears interval and stops timer
-          clearInterval(timer);
-          winGame();
-        }
-      }
-      // Tests if time has run out
-      if (timerCount === 0) {
-        // Clears interval
-        clearInterval(timer);
-        loseGame();
-      }
-    }, 1000);
-  }
-  */
+*/
 
     
-Init();
-buttonEl.addEventListener("click", startGame);
+
 
