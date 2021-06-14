803c962418381d73f55c4cd611992b67a5429ae8
shooters00 <shooters0000@gmail.com>
2021-06-13 20:34:58 -0400

    High scores posted, but need formatted

--- assets/js/highscript.js
+++ assets/js/highscript.js
@@ -2,6 +2,7 @@ var backButtonEl = document.getElementById("goBack");
 var clearButtonEl = document.getElementById("clearHigh");
 var highScoreEl = document.querySelector(".highscoreList")
 
+//If no high scores yet, put that on page.  IF high scores, get them from local storage and output them.
 function Init() {
     highScoreEl.innerhtml = "";
     if (localStorage.getItem("highscores") === null) {
@@ -15,24 +16,42 @@ function Init() {
         var scoreEl;
         var first;
         var number;
+        var highScore = {};
+        //var splitHighScore;
         for (i=0; i < highScores.length; i++) {
             scoreEl = document.createElement("li");
-            highScore = highScores[i].values;
-            //number = highScores.highScore;
+            highScore = highScores[i];
+            scoreEl.textContent = JSON.stringify(highScore);
+            highScoreEl.appendChild(scoreEl);
+
             console.log(highScore);
-            //scoreEl.textContent = first + " " + number;
-            //highScoreEl.appendChild(scoreEl);
-        }
+            console.log(highScore.highName);
+
+            //splitHighScore = highScore.split(",");
+            //console.log(splitHighScore);
+
+            //first = highScore.filter(obj => {
+                //return obj.highName
+            }
+        /*
+        highScores.forEach(element => {
+            console.log(this.highName);
+            console.log(this.highScore);
+        });
+        */
     }
 }
 
+//Initialize high score list
 Init();
 
+//Listen for back button and go back
 backButtonEl.addEventListener("click", function() {
     console.log("Back Button Clicked!");
     window.open('index.html');
 });
 
+//Listen for clear of high scores and clear
 clearButtonEl.addEventListener("click", function() {
     console.log("Clear Highscores Button Clicked!");
 
